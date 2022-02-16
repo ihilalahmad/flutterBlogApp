6d07e34737da966847834547ef5c47943317381d
Hilal <ihilalahmadd@gmail.com>
2022-02-16 23:51:11 +0000

    data fetched from firebase realtime database.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:blog_app_flutter/screens/add_post_screen.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final dbRef = FirebaseDatabase.instance.reference().child('posts');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,18 +21,73 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Home'),
         actions: [
           InkWell(
-              child: Icon(Icons.add),
+            child: Icon(Icons.add),
             onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => AddPostScreen()));
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AddPostScreen()));
             },
           ),
-          SizedBox(width: 20,)
+          SizedBox(width: 10,),
+          InkWell(
+            child: Icon(Icons.logout),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AddPostScreen()));
+            },
+          ),
+          SizedBox(
+            width: 20,
+          )
         ],
       ),
-      body: Column(
-        children: [
-
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: FirebaseAnimatedList(
+                query: dbRef.child('Post List'),
+                itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                    Animation<double> animation, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey.shade100,
+                          borderRadius: BorderRadius.circular(5)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: FadeInImage.assetNetwork(
+                              fit: BoxFit.cover,
+                                width: MediaQuery.of(context).size.width * 1,
+                                height: MediaQuery.of(context).size.height * .25,
+                                placeholder: 'images/firebase_logo.png',
+                                image: snapshot.value['pImage']),
+                          ),
+                          SizedBox(height: 10,),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(snapshot.value['pTitle'],style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(snapshot.value['pDescription'],style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
